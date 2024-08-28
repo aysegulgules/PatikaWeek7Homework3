fcd28a4cd9365954ed9988ba5f2c15d0b7d4eebf
ayşegül güleş <159700271+aysegulgules@users.noreply.github.com>
2024-08-28 13:06:16 +0300

    Update Program.cs

--- PatikaWeek7Homework3/Program.cs
+++ PatikaWeek7Homework3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 
 namespace PatikaWeek7Homework3
 {
@@ -6,24 +6,39 @@ namespace PatikaWeek7Homework3
     {
         static void Main(string[] args)
         {
-            List<Series> seriesList = new List<Series>()
-             {
-                new Series{SeriesName="Avrupa Yakası",ProductionYear=2004,Genre="Komedi",StartedYear=2004,Director="Yüksel Aksu",FirstPlatform="Kanal D" },
-                new Series{SeriesName="Yalan Dünya",ProductionYear=2012,Genre="Komedi",StartedYear=2012,Director="Gülseren Buda Başkaya",FirstPlatform="Fox TV"},
-                new Series{SeriesName="Jet Sosyete",ProductionYear=2018,Genre="Komedi",StartedYear=2018,Director="Gülseren Buda Başkaya",FirstPlatform="Kanal D"},
-                new Series{SeriesName="Dadı",ProductionYear=2006,Genre="Komedi",StartedYear=2006,Director="Yusuf Pirhasan",FirstPlatform="Kanal D"},
-                new Series{SeriesName="Belalı Baldız",ProductionYear=2007,Genre="Komedi",StartedYear=2007,Director="Yüksel Aksu",FirstPlatform="Kanal D"},
-                new Series{SeriesName="Arka Sokaklar",ProductionYear=2004,Genre="Polisiye,Dram",StartedYear=2004,Director="Orhan Oğuz",FirstPlatform="Kanal D"},
-                new Series{SeriesName="Aşk-ı Memnu",ProductionYear=2008,Genre="Dram, Romantik",StartedYear=2008,Director="Hilal Saral",FirstPlatform="Kanal D"},
-                new Series{SeriesName="Muhteşem Yüzyıl",ProductionYear=2011,Genre="Tarihi, Dram",StartedYear=2011,Director="Mercan Çilingiroğlu",FirstPlatform="Kanal D"},
-                new Series{SeriesName="Yaprak Dökümü ",ProductionYear=2006,Genre="Dram",StartedYear=2006,Director="Serdar Akar",FirstPlatform="Kanal D"}
+            List<Series> seriesList = new List<Series>() { } ;
+          
+            bool status=true;
+            while (status)
+            {
 
-             };
+            seriesGo: Console.WriteLine("Yeni bir dizi oluşturmak için istiyor musunuz?(EVET/HAYIR)");
+
+            string seriesStatus= (Console.ReadLine()).ToUpper();
+
+                if (seriesStatus == "EVET")
+                {
+                   Series series = SeriesList();//Dönüş tipi Series olan Metot
+                    seriesList.Add(series);
+                }
+                else if (seriesStatus == "HAYIR")
+                {
+                    status=false;
+                }
+                else 
+                {
+                    Console.WriteLine("Geçersiz giriş.");
+                    goto seriesGo;
+                }
+
+            }
+
+
 
 
             //İlk listede bulunan komedi dizilerinden yeni bir liste oluşturuluyor..
 
-            List<SeriesNew> seriesNews = seriesList.Where(s=>s.Genre.Contains("Komedi"))
+            List<SeriesNew> seriesNews = seriesList.Where(s=>s.Genre.ToLower().Contains("komedi"))
                                                    .Select(s => new SeriesNew { SeriesName=s.SeriesName,Genre=s.Genre,Director=s.Director})
                                                    .ToList();
 
@@ -35,12 +50,34 @@ namespace PatikaWeek7Homework3
                 Console.WriteLine($"Dizi adı..:{s.SeriesName}     Dizi türü..:{s.Genre}     Dizi yönetmeni..:{s.Director}");
             }
 
-            
-            
+
+        }
+        static Series SeriesList()
+        {
+            Series series = new Series();
+
+            Console.WriteLine("Dizinin Adı..:");
+            series.SeriesName=Console.ReadLine();
+
+            Console.WriteLine("Yapım Yılı..:");
+            series.ProductionYear =int.Parse(Console.ReadLine());
 
 
+            Console.WriteLine("Türü..:");
+            series.Genre = Console.ReadLine();
 
 
+            Console.WriteLine("Yayınlamaya Başlama Tarihi..:");
+            series.StartedYear = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Yönetmen..:");
+            series.Director = Console.ReadLine();
+
+            Console.WriteLine("Yayınlandığı ilk Platform..:");
+            series.FirstPlatform = Console.ReadLine();
+
+
+            return series;
         }
     }
 }
